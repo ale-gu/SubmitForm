13f6b03244fd291b258d44e50d676c08256067c0
Alessandra Guglielmana <66109219+ale-gu@users.noreply.github.com>
2020-06-28 10:45:55 +0200

    Update the handleChange function The branch implement the Spread Operator inside the handleChage function: setContact(prevValue => ({ ...preValue, [name]: value }));

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,29 +9,7 @@ function App() {
   function handleChange(event) {
     const { value, name } = event.target;
 
-    setContact(prevValue => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email
-        };
-      } else {
-        if (name === "lName") {
-          return {
-            fName: prevValue.fName,
-            lName: value,
-            email: prevValue.email
-          };
-        } else {
-          return {
-            fName: prevValue.fName,
-            lName: prevValue.lName,
-            email: value
-          };
-        }
-      }
-    });
+    setContact(prevValue => ({ ...preValue, [name]: value }));
   }
 
   const [isOver, setIsOver] = React.useState(false);
